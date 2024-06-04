41ec6acb9c0facf735ae4b83a15ac1e906a8f05a
shersha123 <ukkasnaina99@gmail.com>
2024-06-04 09:44:54 +0530

    Add

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div className="Contact">
+      <h2>Get in Touch</h2>
+      <p>
+        Email  at: <a href="mailto:example@example.com">example@example.com</a>
+      </p>
+      <p>
+        Call  at: <a href="tel:+987456321">+987456321</a>
+      </p>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="App-footer">
+      <p>&copy; 2024 Cognisite Company</p>
+    </footer>
+  );
+}
+
+export default Footer;
